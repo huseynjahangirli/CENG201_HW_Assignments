a7ad0d134c5500a7522c5ee99e78fe91445da9a8
huseyincihangirli <cihangirlihuseyin68@gmail.com>
2025-12-16 11:59:24 +0300

    Initial commit: Patient Class Added

--- src/Patient.java
+++ src/Patient.java
@@ -4,4 +4,15 @@ public class Patient {
     String name;
     int severity;
     int age;
+
+    public Patient(int id, String name, int severity, int age) {
+        this.id = id;
+        this.name = name;
+        this.severity = severity;
+        this.age = age;
+    }
+
+    public String toString() {
+        return "ID: " + id + "\nName: " + name + "\nSeverity: " + severity + "\nAge: " + age;
+    }
 }
