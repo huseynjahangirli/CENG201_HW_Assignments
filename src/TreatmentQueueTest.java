42ffe5e9b7f03c7359af0104e6a6fbe221da82e9
huseyincihangirli <cihangirlihuseyin68@gmail.com>
2025-12-17 21:07:34 +0300

    Initial commit: Add TreatmentQueueTest Class

---
+++ src/TreatmentQueueTest.java
@@ -0,0 +1,32 @@
+public class TreatmentQueueTest {
+    public static void main(String[] args) {
+
+        // Create the queue instance
+        TreatmentQueue newQueue = new TreatmentQueue();
+
+        System.out.println("Enqueueing Patients (Adding 1 to 8)");
+
+        // We must create 'new TreatmentRequest' objects because our method expects objects, not just ints.
+        newQueue.enqueue(new TreatmentRequest(1));
+        newQueue.enqueue(new TreatmentRequest(2));
+        newQueue.enqueue(new TreatmentRequest(3));
+        newQueue.enqueue(new TreatmentRequest(4));
+        newQueue.enqueue(new TreatmentRequest(5));
+        newQueue.enqueue(new TreatmentRequest(6));
+        newQueue.enqueue(new TreatmentRequest(7));
+        newQueue.enqueue(new TreatmentRequest(8));
+
+        // Show queue before deleting
+        newQueue.printQueue();
+
+        // Removing first 3 patients
+        System.out.println("Processing Patients (Dequeue 3 times)");
+        newQueue.dequeue();
+        newQueue.dequeue();
+        newQueue.dequeue();
+
+        // Show remaining queue
+        System.out.println("Remaining Queue");
+        newQueue.printQueue();
+    }
+}
